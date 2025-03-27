e29345d8ac806646c4077ba1eaea3494bbd77568
Arthur Mustafin <mustafinrtz.2000@gmail.com>
2025-03-27 13:28:38 +0300

    [dev] custom decorator and iterator add

---
+++ my_iterator.py
@@ -0,0 +1,17 @@
+class MyIterator:
+    def __init__(self, limit):
+        self.limit = limit
+        self.current = 0
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.current < self.limit:
+            self.current += 1
+            return self.current
+        raise StopIteration
+    
+    
+for num in MyIterator(10):
+    print(num)
