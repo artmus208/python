7cfa6f99840078f00f1372c6196613729d90edea
Arthur Mustafin <mustafinrtz.2000@gmail.com>
2025-03-28 11:52:20 +0300

    [dev] fib iter

---
+++ some_package/fib.py
@@ -0,0 +1,25 @@
+
+class fib_iter():
+    def __init__(self, limit):
+        self.i_prev = 0
+        self.i = 1
+        self.limit = limit
+        self.count = 0
+        
+        
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.count < self.limit:
+            self.current = self.i_prev
+            self.i_prev, self.i = self.i, self.i_prev + self.i
+            self.count += 1
+            return self.current
+        raise StopIteration
+
+
+if __name__ == "__main__":
+    f = fib_iter(100)
+    for i in f:
+        print(i)
